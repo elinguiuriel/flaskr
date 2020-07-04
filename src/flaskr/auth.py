61c460e99c3cc33f059020f89d9ee093a1503897
Pascal Uriel ELINGUI <elinguiuriel@gmail.com>
2020-07-04 10:25:26 +0000

    Adding Test CoverageÂ¶

--- src/flaskr/auth.py
+++ src/flaskr/auth.py
@@ -21,7 +21,7 @@ def register():
         if not username:
             error = 'Username is required.'
         elif not password:
-            error = 'Password is required'
+            error = 'Password is required.'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
